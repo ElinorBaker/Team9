bd1b437bb227e09fff31eece2c1cdfa7bbb21f61
kate.lau <lauk3158@gmail.com>
2021-04-27 19:45:38 +0100

    Sales first attempt

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import csv
 with open('sales.csv', 'r') as sales_csv:
-    spreadsheet = csv.DictReader(sales_csv)
-    for row in spreadsheet:
+    spreadsheet_sales = csv.DictReader(sales_csv)
+    for row in spreadsheet_sales:
         print(dict(row))
+    sales = []
+
+total_sales = sum(sales)
+print('Total Sales: ', total_sales)
