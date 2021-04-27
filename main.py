4ae1d192b65fe98e211f479569ab5075538630f6
meganfletch <meggiemegz13@gmail.com>
2021-04-27 19:52:24 +0100

    List + total sales

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-import csv
-with open('sales.csv', 'r') as sales_csv:
-    spreadsheet_sales = csv.DictReader(sales_csv)
-    for row in spreadsheet_sales:
-        print(dict(row))
-    sales = []
+#import csv
+#with open('sales.csv', 'r') as sales_csv:
+    #spreadsheet_sales = csv.DictReader(sales_csv)
+    #for row in spreadsheet_sales:
+     #   print(dict(row))
+    #sales = []
 
-total_sales = sum(sales)
-print('Total Sales: ', total_sales)
+#total_sales = sum(sales)
+#print('Total Sales: ', total_sales)
+
+sales_list = [6226, 1521, 1842, 2051, 1728, 2138, 7479, 4434, 3615, 5472, 7224, 1812]
+total_sales = sum(sales_list)
+print(total_sales)
